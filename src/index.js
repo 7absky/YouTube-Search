c9f04772a0be119aa4b1aaa7ae94428674a4cd84
Patryk Laba <patryk@labski.it>
2018-01-12 20:58:04 +0100

    Created component files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+const App = function() {
+    return <div>Hello, world!</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
