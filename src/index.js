85f024c6870c3a223c27c1b49e315c9570dc347f
Patryk Laba <patryk@labski.it>
2018-01-12 21:42:22 +0100

    Created SearchBar component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import API from './api_key';
+import SearchBar from './components/SearchBar';
 
 const App = function() {
-    return <div>Hello, world!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
