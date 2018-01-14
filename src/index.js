164e3a49e00cf6551bf11c21b3f7c43fd7f5ea0a
Patryk Laba <patryk@labski.it>
2018-01-14 18:06:42 +0100

    Made App a stateful component

--- src/index.js
+++ src/index.js
@@ -3,15 +3,49 @@ import ReactDOM from 'react-dom';
 import API from './API';
 import './App.css';
 import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
 
-API({q: 'surfboards'}, (data) => console.log(data));
 
-const App = function() {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+        this.handleVideoSelect = this.handleVideoSelect.bind(this);
+
+        API( { q: 'surfboards' }, (videos) => {
+            this.setState( {
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+        
+    }
+
+    handleVideoSelect(video) {
+        console.log('clicked');
+        this.setState({
+            selectedVideo: video
+        });
+    }
+
+    render() {
+        return(
+            <div className="AppWrapper">
+                <SearchBar />
+                <div className="ContentWrapper">
+                    <VideoList 
+                        videos={this.state.videos}
+                        onVideoSelect={this.handleVideoSelect}
+                        />
+                    <VideoDetail video={this.state.selectedVideo} />
+                </div>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
