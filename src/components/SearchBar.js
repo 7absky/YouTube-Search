802ccb0d6d1d2c5317e4ffc769828bd18b38782e
Patryk Laba <patryk@labski.it>
2018-01-14 13:02:44 +0100

    Input as a controlled component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,27 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        }
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+    onInputChange(event) {
+        this.setState({
+            term: event.target.value,
+        });
+    }
     render() {
         return (
-            <input />
-        )
+            <div>
+                <input 
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
+            </div>
+        );
     }
 }
 
